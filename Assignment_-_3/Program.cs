22c198f6f5ac45b38c757996b10e6ba1bb826eb6
Dixesh123 <dixoza4667@gmail.com>
2024-07-12 18:03:22 -0400

    Add project files.

---
+++ Assignment_-_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public decimal RentalPrice { get; set; }
+
+    public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+}
